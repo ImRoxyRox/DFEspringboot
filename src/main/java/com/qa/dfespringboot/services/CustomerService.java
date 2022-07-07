44b2df93ce10948cfb1750089c78b7d49f068894
ImRoxyRox <rossella.ortu@gmail.com>
2022-07-07 14:57:14 +0100

    services

--- src/main/java/com/qa/dfespringboot/services/CustomerService.java
+++ src/main/java/com/qa/dfespringboot/services/CustomerService.java
@@ -1,47 +1,59 @@
 package com.qa.dfespringboot.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.qa.dfespringboot.entities.Customer;
+import com.qa.dfespringboot.repos.CustomerRepo;
 
 @Service
 public class CustomerService {
 
-	private List<Customer> customers = new ArrayList<>();
+	private CustomerRepo repo;
 
-//	POST=CREATE 
+	public CustomerService(CustomerRepo repo) {
+		this.repo = repo;
+	}
+
+//	POST=CREATE saveAndFlush saves immediately, save has delays
 	public Customer create(Customer customer) {
-		this.customers.add(customer);
-		return this.customers.get(this.customers.size() - 1);
+		return this.repo.saveAndFlush(customer);
 
 	}
 
-// GET=READ readAll
+// GET=READ readAll now use findAll
 	public List<Customer> getAll() {
-		return this.customers;
+		return this.repo.findAll();
 	}
 
-//	GET=READ readById
-	public Customer readById(int id) {
-		return this.customers.get(id);
+//	GET=READ readById now use findById
+	public Customer readById(long id) {
+		return this.repo.findById(id).get();
+
 	}
 
 //  PUT=UPDATE
-	public Customer update(int id, Customer customer) {
-		this.customers.remove(id);
-		this.customers.add(id, customer);
-		return this.customers.get(id);
+	public Customer update(long id, Customer customer) {
+//		This gets the existing one
+		Customer existing = this.repo.findById(id).get();
+
+//		To change the existing one with the new customer object, check the notes 
+		existing.setFirstName(customer.getFirstName());
+		existing.setLastName(customer.getLastName());
+		existing.setEmail(customer.getEmail());
+
+//		To save the entry back to the database
+		return this.repo.saveAndFlush(existing);
 
 	}
 
 //  DELETE=DELETE
-	public Customer delete(int id) {
-		return this.customers.remove(id);
+	public boolean delete(long id) {
+		this.repo.deleteById(id);
+
+//		Thsi should be false but with reverse should now be true to confirm deletion
+		return !this.repo.existsById(id);
 	}
 
 }
-
-
